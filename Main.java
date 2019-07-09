bed9526192b81c4f737180b2598198ad26e1e159
Nelson Vargas <nelson101vargas@gmail.com>
2019-07-09 18:55:34 -0400

    Problem 1.1

---
+++ Main.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+public class Main {
+   public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+     // while(true) {
+     //   System.out.println("The result is " + isAllUnique(scan.nextLine()));
+     // }
+
+
+
+   }
+   public static boolean isAllUnique(String sentence) {
+     // char[] letters = new char[sentence.length];
+     // for( int i = 0; i < sentence.length; i ++) {
+     //   letters[i] = sentence.charAt(i);
+     // }
+     // // n +
+     // for (char a: letters) {
+     //   for(char b: letters) {
+     //     if(a.equals(b)) {
+     //       return false;
+     //     }
+     //   }
+     // }
+     return true;
+   }
+}
